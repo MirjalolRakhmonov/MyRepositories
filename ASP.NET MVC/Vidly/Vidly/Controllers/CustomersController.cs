1c7f1b8e48901b19e17c46b5e55ffcbf1fe5fcb2
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-10-08 00:56:14 +0200

    First project from Advanced Programming is created

--- ASP.NET MVC/Vidly/Vidly/Controllers/CustomersController.cs
+++ ASP.NET MVC/Vidly/Vidly/Controllers/CustomersController.cs
@@ -4,6 +4,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+//using System.Web.Security;
 using Vidly.Models;
 using Vidly.ViewModels;
 
@@ -27,11 +28,11 @@ namespace Vidly.Controllers
         {
             var membershipType = _context.MembershipTypes.ToList();
 
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipType
             };
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
@@ -58,6 +59,23 @@ namespace Vidly.Controllers
                 return HttpNotFound();
 
             return View(customer);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c=>c.Id==id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var newCustomerViewModel = new CustomerFormViewModel
+            {
+                Customer=customer,
+                MembershipTypes=_context.MembershipTypes.ToList()
+            };
+            var viewModel = newCustomerViewModel;
+
+            return View("CustomerForm", viewModel);
         }
     }
 }
