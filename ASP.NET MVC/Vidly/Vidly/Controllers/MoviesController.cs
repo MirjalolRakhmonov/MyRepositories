d809e4652850c2f4daaf0bacdaa7ed01df2de7ab
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-10-13 22:54:35 +0200

    Anti-forgery token is Validated

--- ASP.NET MVC/Vidly/Vidly/Controllers/MoviesController.cs
+++ ASP.NET MVC/Vidly/Vidly/Controllers/MoviesController.cs
@@ -88,6 +88,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
             if (!ModelState.IsValid)
