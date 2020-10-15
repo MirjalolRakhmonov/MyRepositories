4dc6d5f1e2c39512975a92d0b39520c9b1290fee
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-10-15 23:31:19 +0200

    Data Transfer Objects

--- ASP.NET MVC/Vidly/Vidly/Dtos/CustomerDto.cs
+++ ASP.NET MVC/Vidly/Vidly/Dtos/CustomerDto.cs
@@ -3,27 +3,22 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using Vidly.Models;
 
-namespace Vidly.Models
+namespace Vidly.Dtos
 {
-    public class Customer
+    public class CustomerDto
     {
-
         public int Id { get; set; }
 
         [Required] // in order to prevent property from being nullable type
         [StringLength(255)]
         public string Name { get; set; }
-        [Display(Name = "Date of Birth")]
+
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
 
-        // Navigation property
-        public MembershipType MembershipType { get; set; }
-
-        // ForeignKey property
-        [Display(Name = "Membership Type")]
-        [Min18YearsIfAMember]
-        public byte MembershipTypeId { get; set; } 
+        public byte MembershipTypeId { get; set; }
     }
 }
