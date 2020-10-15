8d2131804e8c13627c4634299c205b01a71fb4f0
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-10-16 00:20:19 +0200

    AutoMapper is implemented (but not entirely)

---
+++ ASP.NET MVC/Vidly/Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<Customer, CustomerDto>();
+            });
+
+            IMapper mapper = config.CreateMapper();
+            var source = new Customer();
+            var dest = mapper.Map<Customer, CustomerDto>(source);
+            
+            var config2 = new MapperConfiguration(cfg => {
+                cfg.CreateMap<CustomerDto, Customer>();
+            });
+
+            IMapper mapper2 = config.CreateMapper();
+            var source2 = new CustomerDto();
+            var des2t = mapper.Map<CustomerDto, Customer>(source2);
+        }
+    }
+}
